7d1ecba4770ea134d53bda0de4dbf875b2f720e3
Arnold Ruijter <arnoldruijter@Arnolds-MacBook-Pro-2018.local>
2026-05-19 14:46:21 +0200

    autopush: 2026-05-19 14:46:21

--- js/app.js
+++ js/app.js
@@ -317,6 +317,9 @@ const App = {
       case 'export-data':         this.exportData(); break;
       case 'import-data':         document.getElementById('import-file-input')?.click(); break;
       case 'clear-data':          this.clearData(); break;
+      case 'enter-demo':          this.enterDemo(); break;
+      case 'reset-demo':          this.resetDemo(); break;
+      case 'exit-demo':           this.exitDemo(); break;
       case 'force-refresh':       this.forceRefresh(); break;
       case 'star-pick':           this.pickStar(parseInt(args.val, 10)); break;
       case 'type-pick':           this.pickType(args.type); break;
